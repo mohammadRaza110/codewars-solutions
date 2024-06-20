cc1ee1c3c1fe8a3cf0218578e5316bc01ebdd200
mohammadRaza110 <mohammadraza1900@gmail.com>
2024-06-20 18:44:13 +0500

    Feat: - 8 kyu problem uppercase

---
+++ src/8Kyu/p1-uppercase.js
@@ -0,0 +1,11 @@
+/*
+Write a function which converts the input string to uppercase.
+
+https://www.codewars.com/kata/57a0556c7cb1f31ab3000ad7
+*/
+
+function makeUpperCase(str) {
+  return str.toUpperCase();
+}
+
+makeUpperCase("hello");
